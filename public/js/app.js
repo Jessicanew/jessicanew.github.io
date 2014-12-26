3f8dbecfddb578fe3535b38b6bb47e30827a978e
Jessicanew <ww01100110@gmail.com>
2014-12-25 19:38:37 -0800

    Initial import of MyWebsite1 Heroku

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+(function(){
+    "use strict";
+    var mainApp = angular.module("mainApp", ['ui.router']);
+
+}());
---
+++ routes/routing.js
@@ -0,0 +1,16 @@
+angular.module("mainApp").config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("/");
+    $stateProvider.
+    state('main', {
+        url:'/',
+        templateUrl:'/views/main.html'
+    }).
+    state('portfolio', {
+        url:'/portfolio',
+        templateUrl: '/views/portfolio.html'
+    }).
+    state('more', {
+        url: '/moreAboutMe',
+        templateUrl: '/views/about.html'    
+    });     
+});
